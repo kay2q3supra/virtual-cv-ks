ccd6693843bff0079429a5bc7da027c3387adbf6
Kayla Supra <kaylasupra17@gmail.com>
2025-07-25 13:11:44 +0200

    Adding code for the ".js", ".css", ".html" files for VCV hosting of my profile.

--- vcv-script.js
+++ vcv-script.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll("nav ul li a");
 
+  navLinks.forEach(link => {
+    link.addEventListener("click", e => {
+      e.preventDefault();
+      const sectionId = link.getAttribute("href");
+      const section = document.querySelector(sectionId);
+      section.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+});
